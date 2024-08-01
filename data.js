8d2fcfc285fb7259d144b0d411adf2f286fe989d
zidandff <zidandevs@gmail.com>
2024-08-01 08:45:27 +0700

    initial

---
+++ data.js
@@ -0,0 +1,79 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-home",
+      "name": "home",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "yaw": 0.27025688891947297,
+        "pitch": -0.19114382499529725,
+        "fov": 1.3602495036333777
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.326470070328318,
+          "pitch": -0.09461389692713063,
+          "rotation": 0,
+          "target": "1-fossil"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0360921315095997,
+          "pitch": -0.19745055539954137,
+          "title": "Zidaninda",
+          "text": "Text ABC DEdksjdk"
+        }
+      ]
+    },
+    {
+      "id": "1-fossil",
+      "name": "fossil",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
